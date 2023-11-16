92fc7610b076dced655dfde885267772bab51d1f
Huarui Yang <146243657+2333-hr@users.noreply.github.com>
2023-11-16 15:26:43 +0000

    Update codebuzzercodeplan1.ino

--- function code/codebuzzercodeplan1.ino
+++ function code/codebuzzercodeplan1.ino
@@ -1,7 +1,7 @@
 //selected
 const int soilSensorPin = A0;  // Soil moisture sensor connected to analog input pin A0
-const int buzzerPin = 9;      // Buzzer connected to digital output pin 9
-const int thresholdValue = 30;  // Set moisture threshold
+const int buzzerPin = 16;      // Buzzer connected to digital output pin 16
+const int thresholdValue = 300;  // Set moisture threshold
 
 void setup() {
   pinMode(buzzerPin, OUTPUT);
@@ -11,7 +11,7 @@ void setup() {
 void loop() {
   int soilMoisture = analogRead(soilSensorPin);
 
-  if (soilMoisture < thresholdValue) {
+  if (soilMoisture ＞ thresholdValue) {
     // Trigger the buzzer when soil moisture is below the threshold
     digitalWrite(buzzerPin, HIGH);
     delay(1000);  // Delay for 1 second
